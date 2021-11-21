263ddda08120bfcd3037501919883e2c101460ab
MrLuis137 <lalemn2@gmail.com>
2021-11-21 01:03:48 -0600

    Avance en ExecutionAdmin

---
+++ prueba/desktop/src/controller/ExecutionAdmin.java
@@ -0,0 +1,49 @@
+package controller;
+
+import model.Configuration;
+import model.Player;
+
+import java.util.Date;
+
+public class ExecutionAdmin {
+    private Date startTime = new Date(System.currentTimeMillis());
+    private Date lastTime = startTime;
+    private int dayOfYear = 0;
+    private int hours = 0;
+    private Configuration config = Configuration.getInstance();
+    private Player player;
+    private Date injuryStarted;
+
+    ExecutionAdmin(Player player){
+        this.player = player;
+    }
+
+    private void run(){
+        while (true){
+            Date currTime = new Date(System.currentTimeMillis());
+            if(isNextHour()){
+                hours++;
+                player.setEnergy(player.getEnergy() - config.getEnergyDecrease());
+                if(hours == config.getHoursPerDay()){dayOfYear++; hours = 0; }   //AGREGAR MEMENTO
+                if(dayOfYear == config.getDaysPerYear()){player.setAge(player.getAge()+1); dayOfYear = 0;}
+                //NPCAdmin
+                //DiseaseAdmin
+
+            }
+
+            if(player.getInjury() != null){
+                injuryStarted = new Date(System.currentTimeMillis());
+            }
+            else if(player.getInjury() == null && injuryStarted != null){
+                injuryStarted = null;
+            }
+        }
+    }
+
+    private boolean isNextHour(){
+        Date currTime = new Date(System.currentTimeMillis());
+        int difference = (int) (currTime.getTime()- startTime.getTime() / 1000);
+        return (difference >= config.getHourDuration())? true:false;
+    }
+
+}
--- prueba/desktop/src/model/Configuration.java
+++ prueba/desktop/src/model/Configuration.java
@@ -1,4 +1,52 @@
 package model;
 
 public class Configuration {
+
+    private static Configuration instance;
+    private int hourDuration = 5;
+    private int hoursPerDay = 24;
+    private int daysPerYear = 10;
+    //20 minutos para que pase un año con los defaults.
+    // ***ACTUALIZAR EL TOTAL SI SE CAMBIAN***
+    private int energyDecrease = 1;
+
+    public int getHoursPerDay() {
+        return hoursPerDay;
+    }
+
+    public void setHoursPerDay(int hoursPerDay) {
+        this.hoursPerDay = hoursPerDay;
+    }
+
+    public int getDaysPerYear() {
+        return daysPerYear;
+    }
+
+    public void setDaysPerYear(int daysPerYear) {
+        this.daysPerYear = daysPerYear;
+    }
+
+    public int getEnergyDecrease() {
+        return energyDecrease;
+    }
+
+    public void setEnergyDecrease(int energyDecrease) {
+        this.energyDecrease = energyDecrease;
+    }
+
+    private Configuration(){
+
+    }
+
+    public static Configuration getInstance(){
+        return (instance == null)? new Configuration(): instance;
+    }
+
+    public int getHourDuration() {
+        return hourDuration;
+    }
+
+    public void setHourDuration(int hourDuration) {
+        this.hourDuration = hourDuration;
+    }
 }
