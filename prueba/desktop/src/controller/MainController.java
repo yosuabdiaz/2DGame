862c89ecbbaa763d95b06b8c5adb511074b212c9
Esteban <estebanguzra@gmail.com>
2021-11-25 13:39:58 -0600

    Update main controller, arraylist -> hashmap (actions)

--- prueba/desktop/src/controller/MainController.java
+++ prueba/desktop/src/controller/MainController.java
@@ -7,38 +7,38 @@ import model.actions.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class MainController {//I need a new name
     private Player player;
     private Storage storage;
-    private ArrayList<Action> actions;
+    private HashMap<String ,Action> actions;
     private ArrayList<Sport> sports;
 
     public MainController() {
         //All init stuff
         player = new Player();
         storage = new Storage();
-        actions = new ArrayList<Action>();
+        actions = new HashMap<String, Action>();
         addActions();
         sports = new ArrayList<Sport>();
         addSports();
     }
 
-    public void executeAction(HashMap<String,GameContex> context, int indexAction){
-        actions.get(indexAction).execute(context);
+    public void executeAction(HashMap<String,GameContex> context, String nameAction){
+        context.put("player", (GameContex) player);
+        actions.get(nameAction).execute(context);
     }
     private void addActions(){
-        actions.add(new DoNothingAction()); //0
-        actions.add(new EatAction());//1
-        actions.add(new GatherAction());//2
-        actions.add(new GoBathroomAction());//3
-        actions.add(new GoFightAction());//4
-        actions.add(new MeditationAction());//5
-        actions.add(new SleepAction());//6
-        actions.add(new SocializeAction());//7
-        actions.add(new ToStockAction());//8 kitchen
-        actions.add(new WorkoutAction());//9
+        actions.put("DoNothing", new DoNothingAction());
+        actions.put("Eat", new EatAction());
+        actions.put("Gather", new GatherAction());
+        actions.put("GoBathroom", new GoBathroomAction());
+        actions.put("GoFight", new GoFightAction());
+        actions.put("Meditation", new MeditationAction());
+        actions.put("Sleep", new SleepAction());
+        actions.put("Socialize", new SocializeAction());
+        actions.put("ToStock", new ToStockAction());
+        actions.put("Workout", new WorkoutAction());
     }
     private void addSports(){
         //
@@ -48,31 +48,11 @@ public class MainController {//I need a new name
         return player;
     }
 
-    public void setPlayer(Player player) {
-        this.player = player;
-    }
-
     public Storage getStorage() {
         return storage;
     }
 
-    public void setStorage(Storage storage) {
-        this.storage = storage;
-    }
-
-    public ArrayList<Action> getActions() {
-        return actions;
-    }
-
-    public void setActions(ArrayList<Action> actions) {
-        this.actions = actions;
-    }
-
     public ArrayList<Sport> getSports() {
         return sports;
-    }
-
-    public void setSports(ArrayList<Sport> sports) {
-        this.sports = sports;
     }
 }
