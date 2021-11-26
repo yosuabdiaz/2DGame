478030481f9aea3eccc5b3e1f31c3ed2d3609451
Esteban <estebanguzra@gmail.com>
2021-11-25 23:43:02 -0600

    Fix getAll with details of amount

--- prueba/desktop/src/model/Storage.java
+++ prueba/desktop/src/model/Storage.java
@@ -4,32 +4,50 @@ import model.actions.GameContex;
 import com.badlogic.gdx.utils.Array;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Storage implements GameContex {
     private ArrayList<Food> food;
     private ArrayList<Medicine> medicine;
+    private HashMap<String, Integer> amountFood;
+    private HashMap<String, Integer> amountMedicine;
 
     public Storage(){
         food = new ArrayList<Food>();
         medicine = new ArrayList<Medicine>();
+        amountFood = new HashMap<String, Integer>();
+        amountMedicine = new HashMap<String, Integer>();
     }
 
     public void addFood(Food food){
-        this.food.add(food);
+        if(amountFood.get(food.getName()) == null) {
+            this.food.add(food);
+            amountFood.put(food.getName(), 1);
+        } else {
+            amountFood.put(food.getName(), amountFood.get(food.getName()) + 1);
+        }
     }
 
     public void addMedicine(Medicine medicine){
-        this.medicine.add(medicine);
+        if(amountMedicine.get(medicine.getName()) == null) {
+            this.medicine.add(medicine);
+            amountMedicine.put(medicine.getName(), 1);
+        } else {
+            amountMedicine.put(medicine.getName(), amountMedicine.get(medicine.getName()) + 1);
+        }
     }
 
     public Array<String> getAll(){
         Array<String> array = new Array<String>();
-        for(Food food:food){
-            array.add(food.getName());
+        ArrayList<String> keysFood = new ArrayList<>(amountFood.keySet());
+        for(String key:keysFood){
+            array.add("Comida:" + key + ": "+ amountFood.get(key));
         }
-        for(Medicine medicine:medicine){
-            array.add(medicine.getName());
+        ArrayList<String> keysMedicine = new ArrayList<>(amountMedicine.keySet());
+        for(String key:keysMedicine){
+            array.add("Medicina:" + key + ": "+ amountMedicine.get(key));
         }
+
         return array;
     }
 }
