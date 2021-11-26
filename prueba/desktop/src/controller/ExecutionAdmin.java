44e019234e0175575534a6ec900bfdaa5fea7936
MrLuis137 <lalemn2@gmail.com>
2021-11-26 12:27:17 -0600

    Revert "Minutes added"
    This reverts commit f713b11e573d61756324fae5bed7e8034ec6f2c1.

--- prueba/desktop/src/controller/ExecutionAdmin.java
+++ prueba/desktop/src/controller/ExecutionAdmin.java
@@ -8,7 +8,6 @@ import java.util.Random;
 public class ExecutionAdmin extends Thread{
     private Date startTime = new Date(System.currentTimeMillis());
     private int dayOfYear = 0;
-    private int minutes = 0;
     private int hours = 0;
     private Configuration config = Configuration.getInstance();
     private Player player;
@@ -27,34 +26,32 @@ public class ExecutionAdmin extends Thread{
         Random rand = new Random();
         while (true) {
             try {
-                Thread.sleep((config.getHourDuration() * 1000)/4);
+                Thread.sleep(config.getHourDuration() * 1000);
+                //System.out.println("esperado");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            minutes++;
-            if(minutes > 3){
-                minutes = 0;
-                hours++;
-                if(player.getEnergy() > 1) {
-                    player.setEnergy(player.getEnergy() - config.getEnergyDecrease());
-                }
-                if (hours == config.getHoursPerDay()) {
-                    dayOfYear++;
-                    hours = 0;
-                    garden.growGarden();
-                    mementoAdmin.addMemento(new Memento(player, storage));
-                    NPCAdmin.setAttacksToDay(0);
-                    NPCAdmin.setVisited(false);
-                    player.setMeditation(0);
-                }
-                if (dayOfYear == config.getDaysPerYear()) {
-                    player.setAge(player.getAge() + 1);
-                    dayOfYear = 0;
-                }
-                int npcProbability = rand.nextInt(100);
-                if (npcProbability < 2) {
-                    NPCAdmin.generateNPC(player);
-                }
+
+            hours++;
+            if(player.getEnergy() > 1) {
+                player.setEnergy(player.getEnergy() - config.getEnergyDecrease());
+            }
+            if (hours == config.getHoursPerDay()) {
+                dayOfYear++;
+                hours = 0;
+                garden.growGarden();
+                mementoAdmin.addMemento(new Memento(player, storage));
+                NPCAdmin.setAttacksToDay(0);
+                NPCAdmin.setVisited(false);
+                player.setMeditation(0);
+            }
+            if (dayOfYear == config.getDaysPerYear()) {
+                player.setAge(player.getAge() + 1);
+                dayOfYear = 0;
+            }
+            int npcProbability = rand.nextInt(100);
+            if (npcProbability < 2) {
+                NPCAdmin.generateNPC(player);
             }
 
 
@@ -79,55 +76,5 @@ public class ExecutionAdmin extends Thread{
         }
     }
 
-    public int getDayOfYear() {
-        return dayOfYear;
-    }
 
-    public void setDayOfYear(int dayOfYear) {
-        this.dayOfYear = dayOfYear;
-    }
-
-    public int getMinutes() {
-        return minutes * 15;
-    }
-
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    public void setConfig(Configuration config) {
-        this.config = config;
-    }
-
-    public void setPlayer(Player player) {
-        this.player = player;
-    }
-
-    public void setInjuryStarted(Date injuryStarted) {
-        this.injuryStarted = injuryStarted;
-    }
-
-    public void setMementoAdmin(MementoAdmin mementoAdmin) {
-        this.mementoAdmin = mementoAdmin;
-    }
-
-    public void setStorage(Storage storage) {
-        this.storage = storage;
-    }
-
-    public void setGarden(Garden garden) {
-        this.garden = garden;
-    }
 }
