0819c2529ceff34f427368c34526367375017917
Esteban <estebanguzra@gmail.com>
2021-11-27 01:10:12 -0600

    socialize action

--- prueba/desktop/src/model/actions/SocializeAction.java
+++ prueba/desktop/src/model/actions/SocializeAction.java
@@ -13,7 +13,17 @@ public class SocializeAction extends Action{
     @Override
     public void execute(HashMap<String, GameContex> contex) {
         Player player = (Player) contex.get("player");
+        updateStats(player);
         render();
+    }
+
+    private void updateStats(Player player) {
+        int newMentalHealth = player.getMentalHealth() < 80 ? player.getMentalHealth() + 20 : 100;
+        player.setMentalHealth(newMentalHealth);
+        int newEnergy = player.getEnergy() > 5 ? player.getEnergy() - 5 : 1;
+        player.setEnergy(newEnergy);
+        int newSleep = player.getSleep() < 95 ? player.getSleep() + 5 : 99;
+        player.setSleep(newSleep);
     }
 
     @Override
