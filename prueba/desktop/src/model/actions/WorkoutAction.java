a6b37c867c2575e7ab9c4106cf7d57a07a92be94
Esteban <estebanguzra@gmail.com>
2021-11-26 23:36:28 -0600

    get attack from sport in workout

--- prueba/desktop/src/model/actions/WorkoutAction.java
+++ prueba/desktop/src/model/actions/WorkoutAction.java
@@ -3,9 +3,11 @@ package model.actions;
 import View.GameScreen;
 import View.mainView;
 import com.badlogic.gdx.graphics.Texture;
+import model.Attack;
 import model.Player;
 import model.Sport;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class WorkoutAction extends Action{
@@ -15,7 +17,18 @@ public class WorkoutAction extends Action{
     public void execute(HashMap<String, GameContex> contex) {
         Player player = (Player) contex.get("player");
         Sport sport = (Sport) contex.get("sport");
+        getAttack(sport,player);
         render();
+    }
+
+    private void getAttack(Sport sport, Player player) {
+        ArrayList<Attack> currentList = player.getAttackSkills();
+        for(int i = 0; i < sport.getLoot().size(); i++){
+            if(!currentList.contains(sport.getLoot().get(i))){
+                player.addAttack(sport.getLoot().get(i));
+                break;
+            }
+        }
     }
 
     @Override
