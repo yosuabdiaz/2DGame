1173400afc1c49ac62832c3948d21da658c12510
Esteban <estebanguzra@gmail.com>
2021-11-26 23:58:40 -0600

    getInjury in workout action

--- prueba/desktop/src/model/actions/WorkoutAction.java
+++ prueba/desktop/src/model/actions/WorkoutAction.java
@@ -4,11 +4,13 @@ import View.GameScreen;
 import View.mainView;
 import com.badlogic.gdx.graphics.Texture;
 import model.Attack;
+import model.Injury;
 import model.Player;
 import model.Sport;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class WorkoutAction extends Action{
     public WorkoutAction() {}
@@ -18,7 +20,19 @@ public class WorkoutAction extends Action{
         Player player = (Player) contex.get("player");
         Sport sport = (Sport) contex.get("sport");
         getAttack(sport,player);
+        getInjury(sport,player);
         render();
+    }
+
+    private void getInjury(Sport sport, Player player) {
+        Random random = new Random();
+        float ticket = (float) random.nextInt(99) + 1; //1-100
+        if(ticket > sport.getInjuryProb()){//get injury :(
+            int daysInjury = random.nextInt(4) + 1;
+            String nameInjury = "Injury of " + sport.getName() + "lv" + daysInjury;
+            float speedReduction = random.nextFloat();
+            player.setInjury(new Injury(nameInjury,daysInjury,speedReduction));
+        }
     }
 
     private void getAttack(Sport sport, Player player) {
