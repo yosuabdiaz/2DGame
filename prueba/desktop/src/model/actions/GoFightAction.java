08e21aa2ce0be7aecf94384259d164d6deae45a8
Esteban <estebanguzra@gmail.com>
2021-11-27 21:25:01 -0600

    figth method

--- prueba/desktop/src/model/actions/GoFightAction.java
+++ prueba/desktop/src/model/actions/GoFightAction.java
@@ -15,10 +15,30 @@ public class GoFightAction extends Action  {
     public void execute(HashMap<String, GameContex> contex) {
         Player player = (Player) contex.get("player");
         EnemyPlayer enemy = (EnemyPlayer) contex.get("enemy");
+        figth(player,enemy);
         //Dialogo para seleccionar
         mainView.getInstance().getMyGameScreen().selectAttack();
         //Automatizar
         render();
+    }
+
+    private void figth(Player player, EnemyPlayer enemy) {
+        int playerPoints = 0;
+        int enemyPoints = 0;
+        while(playerPoints < 100 || enemyPoints < 100){
+
+        }
+        if(playerPoints > enemyPoints){
+            winPlayer(player, enemy);
+        } else {
+            losePlayer(player);
+        }
+    }
+
+    private void winPlayer(Player player, EnemyPlayer enemy) {
+    }
+
+    private void losePlayer(Player player) {
     }
 
     @Override
